fc2b31ebeda60023a1e7c2ab211b8464c1a9a046
Thomas Wilson <wilf06@hotmail.co.uk>
2021-01-26 16:01:50 +0100

    chat working

--- client/src/chat.js
+++ client/src/chat.js
@@ -1,14 +1,31 @@
 import { useSelector } from "react-redux";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { socket } from "./socket";
 
 export default function Chat() {
-    const chatMessages = useSelector((state) => state && state.messages);
+    // const chatMessages = useSelector((state) => state && state.messages);
     const [signIn, setSignIn] = useState(false);
     const [userName, setUserName] = useState("");
     const [messages, setNewMessages] = useState([]);
 
-    // console.log("chatmsg: ", chatMessages);
+    var elemRef = useRef();
+    // document.getElementsByClassName("chat-container");
+
+    useEffect(() => {
+        socket.on("chat_message", (message) => {
+            setNewMessages([...messages, message]);
+        });
+        if (messages.length > 0) {
+            const chat = document.getElementById("chat-container");
+            chat.scrollTo(0, 1000);
+        }
+
+        // // elemRef.current.scrollTo(0, 1000);
+        return () => {
+            socket.off("chat_message");
+        };
+    }, [messages]);
+
     const handleKeyDown = (e) => {
         if (e.key === "Enter") {
             console.log("enter has been pressed");
@@ -18,31 +35,19 @@ export default function Chat() {
             e.target.value = null;
         }
     };
-    const elemRef = useRef();
+
     const handleNewMessage = (e) => {
         if (e.key === "Enter") {
+            e.preventDefault();
             console.log("enter has been pressed");
             socket.emit("chat_message", {
                 message: e.target.value,
                 user: userName,
             });
             e.target.value = null;
+            e.target.setSelectionRange(0, 0);
         }
     };
-
-    socket.on("chat_message", (data) => {
-        console.log("new msg rcvd: ", data);
-        setNewMessages(...messages, data);
-        console.log("messages after setting: ", messages);
-    });
-
-    // if (!socket.session) {
-    //     var username = prompt("please enter your username");
-    //     socket.emit("username", username);
-    // } else {
-    //     console.log("username: ", socket.username);
-    // }
-    console.log("messages after setting 2: ", messages);
 
     return (
         <>
@@ -51,14 +56,16 @@ export default function Chat() {
             )}
             <h1 className="bodyTextCol">Welcome to the chat {userName}</h1>
             {signIn && (
-                <div className="chat-container" ref={elemRef}>
-                    {messages.map((data, i) => (
-                        <div className="user" key={i}>
-                            <p>
-                                {data.user} - {data.message}
-                            </p>
-                        </div>
-                    ))}
+                <div id="chat-container" ref={elemRef}>
+                    {messages &&
+                        messages.map((data, i) => (
+                            <div className="user" key={i}>
+                                <p>
+                                    {data.user} - {data.message}
+                                </p>
+                            </div>
+                        ))}
+
                     <textarea onKeyDown={handleNewMessage} />
                 </div>
             )}
